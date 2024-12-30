37ca338f973a80898bf685476de282b26c9f4f11
Mounir <polomnr1@gmail.com>
2024-12-30 14:45:49 +0100

    write tests

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,58 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
-import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:quizy/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Start quiz', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Tap the 'Start Quiz' button.
+    await tester.tap(find.text('Start Quiz'));
+    await tester.pumpAndSettle();
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    // Verify that we navigated to the quiz page.
+    expect(find.text('Question 1 of 6'), findsOneWidget);
+  });
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+  testWidgets('Answer questions', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    // Tap the 'Start Quiz' button.
+    await tester.tap(find.text('Start Quiz'));
+    await tester.pumpAndSettle();
+
+    // Answer the first question.
+    await tester.tap(find.text('True'));
+    await tester.pumpAndSettle();
+
+    // Verify that we navigated to the next question.
+    expect(find.text('Question 2 of 6'), findsOneWidget);
+
+    // Answer the second question.
+    await tester.tap(find.text('True'));
+    await tester.pumpAndSettle();
+
+    // Verify that we navigated to the next question.
+    expect(find.text('Question 3 of 6'), findsOneWidget);
+  });
+
+  testWidgets('Complete quiz', (WidgetTester tester) async {
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(const MyApp());
+
+    // Tap the 'Start Quiz' button.
+    await tester.tap(find.text('Start Quiz'));
+    await tester.pumpAndSettle();
+
+    // Answer all questions.
+    for (var i = 0; i < 6; i++) {
+      await tester.tap(find.text('True'));
+      await tester.pumpAndSettle();
+    }
+
+    // Verify that we navigated to the score screen.
+    expect(find.text('You scored 5 out of 6'), findsOneWidget);
   });
 }
