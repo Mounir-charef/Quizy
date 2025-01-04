2fa0fcc4922a0e1b65751467ac458bed6edd59a2
Mounir <polomnr1@gmail.com>
2025-01-04 14:19:55 +0100

    refactor main

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import 'screens/home_page.dart';
-import 'screens/quiz_page.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({Key? key}) : super(key: key);
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
